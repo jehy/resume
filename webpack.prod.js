a183ed7267aa15b96ad812fb04a605efa8e8071a
Jehy <fate@jehy.ru>
2020-07-24 01:40:50 +0300

    updated job details and deps

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,18 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   devtool: 'source-map',
   cache: true,
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/CNAME', to: '' },
+      ],
+    }),
     new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(ru|en)$/), // https://github.com/webpack/webpack/issues/87
     new BundleAnalyzerPlugin({analyzerMode: 'static', openAnalyzer: false}),
     new ExtractTextPlugin('styles.css'),
